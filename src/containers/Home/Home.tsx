4ed528e31f7e4fdc3b512db4350307831a74928f
Alberto_Callegari <alberto.callegari@nttdata.com>
2022-12-12 17:58:30 +0100

    feat: add router

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import "./Home.scss";
+
+const Home = (): JSX.Element => {
+  return (
+    <div className="home">
+      <p>Bubble Chart</p>
+    </div>
+  );
+};
+
+export default Home;
