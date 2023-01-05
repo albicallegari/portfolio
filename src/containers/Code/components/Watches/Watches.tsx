8fcf80291c54cd6adf8a1b11e2d3c2ef5014abc1
Alberto_Callegari <alberto.callegari@nttdata.com>
2023-01-05 18:19:58 +0100

    feat: color clock

--- src/containers/Code/components/Watches/Watches.tsx
+++ src/containers/Code/components/Watches/Watches.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import useWindowDimensions from "../../../../hooks/useWindowDimensions/useWindowDimensions";
 import vars from "../../../../styles/variables.scss";
 import { Box, useMediaQuery } from "@mui/material";
@@ -7,44 +8,52 @@ import { useEffect, useState } from "react";
 const Watches = () => {
   const { height: windowHeight, width: windowWidth } = useWindowDimensions();
   const isTablet = useMediaQuery(`(min-width:${vars["breakpoint-md"]})`);
-  const [hours, setHours] = useState<number>();
-  const [minutes, setMinutes] = useState<number>();
-  const [seconds, setSeconds] = useState<number>();
+  const [hours, setHours] = useState<string>();
+  const [minutes, setMinutes] = useState<string>();
+  const [seconds, setSeconds] = useState<string>();
   const [ampm, setAmpm] = useState<string>();
 
   const percentage = (partialValue: number, totalValue: number) => {
     return (partialValue * totalValue) / 100;
   };
 
+  const formatSingleNumber = (n: number): string => {
+    if (n < 10) {
+      return `0${n}`;
+    }
+    return `${n}`;
+  };
+
   const refreshClock = () => {
     const h = new Date().getHours();
     const m = new Date().getMinutes();
     const s = new Date().getSeconds();
-    setHours(h);
-    setMinutes(m);
-    setSeconds(s)
+    setMinutes(formatSingleNumber(m));
+    setSeconds(formatSingleNumber(s));
+    if (h > 12) {
+      setHours(formatSingleNumber(h - 12));
+    } else setHours(formatSingleNumber(h));
     if (h >= 12) {
-      setAmpm('PM')
-    } else setAmpm('AM'); 
-  }
+      setAmpm("PM");
+    } else setAmpm("AM");
+  };
 
   useEffect(() => {
     const h = new Date().getHours();
     const m = new Date().getMinutes();
     const s = new Date().getSeconds();
-    setHours(h);
-    setMinutes(m);
-    setSeconds(s);
+    setMinutes(formatSingleNumber(m));
+    setSeconds(formatSingleNumber(s));
     if (h > 12) {
-      setHours(h - 12);
-    } else setHours(h)
-;    if (h >= 12) {
-      setAmpm('PM')
-    } else setAmpm('AM'); 
+      setHours(formatSingleNumber(h - 12));
+    } else setHours(formatSingleNumber(h));
+    if (h >= 12) {
+      setAmpm("PM");
+    } else setAmpm("AM");
     const timerId = setInterval(refreshClock, 1000);
-    return  () => {
+    return () => {
       clearInterval(timerId);
-    }
+    };
   }, []);
 
   return (
@@ -66,15 +75,54 @@ const Watches = () => {
       }}
     >
       <div className="watches_circle">
+        <svg>
+          <circle cx="70" cy="70" r="70"></circle>
+          <circle
+            style={{
+              strokeDashoffset:
+                new Date().getHours() > 12
+                  ? 440 - (440 * (new Date().getHours() - 12)) / 12
+                  : 440 - (440 * new Date().getHours()) / 12,
+            }}
+            cx="70"
+            cy="70"
+            r="70"
+            id="hh"
+          ></circle>
+        </svg>
         <div id="hour">{hours}</div>
       </div>
       <div className="watches_circle">
+        <svg>
+          <circle cx="70" cy="70" r="70"></circle>
+          <circle
+            style={{
+              strokeDashoffset: 440 - (440 * new Date().getMinutes()) / 60,
+            }}
+            cx="70"
+            cy="70"
+            r="70"
+            id="mm"
+          ></circle>
+        </svg>
         <div id="minutes">{minutes}</div>
       </div>
       <div className="watches_circle">
+        <svg>
+          <circle cx="70" cy="70" r="70"></circle>
+          <circle
+            style={{
+              strokeDashoffset: 440 - (440 * new Date().getSeconds()) / 60,
+            }}
+            cx="70"
+            cy="70"
+            r="70"
+            id="ss"
+          ></circle>
+        </svg>
         <div id="seconds">{seconds}</div>
       </div>
-      <div className="ap">
+      <div className="watches_ap">
         <div className="ampm">{ampm}</div>
       </div>
     </Box>
