b0e36a3343ad3dba6a69a36ec39847031b7bb386
Alberto_Callegari <alberto.callegari@nttdata.com>
2023-03-29 17:03:45 +0200

    feat: change img format

--- src/containers/Bloodsucker/Bloodsucker.utils.ts
+++ src/containers/Bloodsucker/Bloodsucker.utils.ts
@@ -1,16 +1,19 @@
 import { SxProps, Theme } from "@mui/material";
 import { BloodsuckerItem } from "./Bloodsucker.models";
-import cap11 from "../../assets/shop/caps/cap11.png";
-import cap12 from "../../assets/shop/caps/cap12.png";
-import cap13 from "../../assets/shop/caps/cap13.png";
-import cap21 from "../../assets/shop/caps/cap21.png";
-import cap22 from "../../assets/shop/caps/cap22.png";
-import cap23 from "../../assets/shop/caps/cap23.png";
-import cap31 from "../../assets/shop/caps/cap31.png";
-import cap41 from "../../assets/shop/caps/cap41.png";
-import tee11 from "../../assets/shop/tees/tee11.png";
-import tee12 from "../../assets/shop/tees/tee12.png";
-import tee13 from "../../assets/shop/tees/tee13.png";
+import cap11 from "../../assets/shop/caps/cap11.webp";
+import cap12 from "../../assets/shop/caps/cap12.webp";
+import cap13 from "../../assets/shop/caps/cap13.webp";
+import cap21 from "../../assets/shop/caps/cap21.webp";
+import cap22 from "../../assets/shop/caps/cap22.webp";
+import cap23 from "../../assets/shop/caps/cap23.webp";
+import cap31 from "../../assets/shop/caps/cap31.webp";
+import cap41 from "../../assets/shop/caps/cap41.webp";
+import tee11 from "../../assets/shop/tees/tee11.webp";
+import tee12 from "../../assets/shop/tees/tee12.webp";
+import tee13 from "../../assets/shop/tees/tee13.webp";
+import tee21 from "../../assets/shop/tees/tee21.webp";
+import tee22 from "../../assets/shop/tees/tee22.webp";
+import tee23 from "../../assets/shop/tees/tee23.webp";
 
 export const getHeaderStyles = (): SxProps<Theme> => {
   return {
@@ -45,6 +48,7 @@ export const getItemsList = (): BloodsuckerItem[] => {
       name: "Bloodsucker Tee",
       colors: [
         { code: "BSTw00", cCode: "#fff", label: "White", imgs: [tee11, tee12, tee13] },
+        { code: "BSTb00", cCode: "#202020", label: "Black", imgs: [tee21, tee22, tee23] },
       ],
       sizes: ["S", "M", "L", "XL", "2XL"],
       price: "24,90€",
